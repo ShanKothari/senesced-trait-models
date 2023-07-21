fdb529910cb249af4c4915f0b88ffc7ba8c38ce0
Shan Kothari <quercusacerifolia@gmail.com>
2023-07-21 13:34:30 -0400

    change NDF/ADF to sol/hemi/recalc

--- 02A plsr_build_intact.R
+++ 02A plsr_build_intact.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/kotha020/Dropbox/TraitModels2018/SenescencePaper/")
+setwd("C:/Users/querc/Dropbox/TraitModels2018/SenescencePaper/")
 library(spectrolab)
 library(patchwork)
 library(pls)
@@ -16,9 +16,11 @@ source("Scripts/senesced-trait-models/useful_functions.R")
 #########################################
 ## train models
 
-NDF_intact<-plsr(meta(intact_spec_agg_train)$NDF~as.matrix(intact_spec_agg_train),
+sol_intact<-plsr(meta(intact_spec_agg_train)$solubles~as.matrix(intact_spec_agg_train),
                  ncomp=30,method = "oscorespls",validation="CV",segments=10)
-ADF_intact<-plsr(meta(intact_spec_agg_train)$ADF~as.matrix(intact_spec_agg_train),
+hemi_intact<-plsr(meta(intact_spec_agg_train)$hemicellulose~as.matrix(intact_spec_agg_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+recalc_intact<-plsr(meta(intact_spec_agg_train)$recalcitrant~as.matrix(intact_spec_agg_train),
                  ncomp=30,method = "oscorespls",validation="CV",segments=10)
 perN_intact<-plsr(meta(intact_spec_agg_train)$perN~as.matrix(intact_spec_agg_train),
                   ncomp=30,method = "oscorespls",validation="CV",segments=10)
@@ -31,14 +33,14 @@ perN_area_intact<-plsr(meta(intact_spec_agg_train)$perN_area~as.matrix(intact_sp
 perC_area_intact<-plsr(meta(intact_spec_agg_train)$perC_area~as.matrix(intact_spec_agg_train),
                        ncomp=30,method = "oscorespls",validation="CV",segments=10)
 
-ncomp_NDF_intact <- selectNcomp(NDF_intact, method = "onesigma", plot = FALSE)
-NDF_intact_valid <- which(!is.na(meta(intact_spec_agg_train)$NDF))
-NDF_intact_pred<-data.frame(ID=meta(intact_spec_agg_train)$ID[NDF_intact_valid],
-                            Species=meta(intact_spec_agg_train)$sp[NDF_intact_valid],
-                            Run=meta(intact_spec_agg_train)$FiberRun[NDF_intact_valid],
-                            Measured=meta(intact_spec_agg_train)$NDF[NDF_intact_valid],
-                            val_pred=NDF_intact$validation$pred[,,ncomp_NDF_intact])
-ggplot(NDF_intact_pred,aes(x=Measured,y=val_pred,color=Species))+
+ncomp_sol_intact <- selectNcomp(sol_intact, method = "onesigma", plot = FALSE)
+sol_intact_valid <- which(!is.na(meta(intact_spec_agg_train)$solubles))
+sol_intact_pred<-data.frame(ID=meta(intact_spec_agg_train)$ID[sol_intact_valid],
+                            Species=meta(intact_spec_agg_train)$sp[sol_intact_valid],
+                            Run=meta(intact_spec_agg_train)$FiberRun[sol_intact_valid],
+                            Measured=meta(intact_spec_agg_train)$solubles[sol_intact_valid],
+                            val_pred=sol_intact$validation$pred[,,ncomp_sol_intact])
+ggplot(sol_intact_pred,aes(x=Measured,y=val_pred,color=Species))+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   theme_bw()+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
@@ -46,16 +48,33 @@ ggplot(NDF_intact_pred,aes(x=Measured,y=val_pred,color=Species))+
   theme(text = element_text(size=20),
         legend.position = c(0.8, 0.2))+
   labs(x="Measured",y="Predicted")+
-  ggtitle("Predicting %NDF from intact-leaf spectra")+guides(color=F)
+  ggtitle("Predicting % solubles from intact-leaf spectra")+guides(color=F)
 
-ncomp_ADF_intact <- selectNcomp(ADF_intact, method = "onesigma", plot = FALSE)
-ADF_intact_valid <- which(!is.na(meta(intact_spec_agg_train)$ADF))
-ADF_intact_pred<-data.frame(ID=meta(intact_spec_agg_train)$ID[ADF_intact_valid],
-                            Species=meta(intact_spec_agg_train)$sp[ADF_intact_valid],
-                            Run=meta(intact_spec_agg_train)$FiberRun[ADF_intact_valid],
-                            Measured=meta(intact_spec_agg_train)$ADF[ADF_intact_valid],
-                            val_pred=ADF_intact$validation$pred[,,ncomp_ADF_intact])
-ggplot(ADF_intact_pred,aes(x=Measured,y=val_pred,color=Species))+
+ncomp_hemi_intact <- selectNcomp(hemi_intact, method = "onesigma", plot = FALSE)
+hemi_intact_valid <- which(!is.na(meta(intact_spec_agg_train)$hemicellulose))
+hemi_intact_pred<-data.frame(ID=meta(intact_spec_agg_train)$ID[hemi_intact_valid],
+                            Species=meta(intact_spec_agg_train)$sp[hemi_intact_valid],
+                            Run=meta(intact_spec_agg_train)$FiberRun[hemi_intact_valid],
+                            Measured=meta(intact_spec_agg_train)$hemicellulose[hemi_intact_valid],
+                            val_pred=hemi_intact$validation$pred[,,ncomp_hemi_intact])
+ggplot(hemi_intact_pred,aes(x=Measured,y=val_pred,color=Species))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  coord_cartesian(xlim=c(25,75),ylim=c(25,75))+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(x="Measured",y="Predicted")+
+  ggtitle("Predicting % hemicellulose from intact-leaf spectra")+guides(color=F)
+
+ncomp_recalc_intact <- selectNcomp(recalc_intact, method = "onesigma", plot = FALSE)
+recalc_intact_valid <- which(!is.na(meta(intact_spec_agg_train)$recalcitrant))
+recalc_intact_pred<-data.frame(ID=meta(intact_spec_agg_train)$ID[recalc_intact_valid],
+                            Species=meta(intact_spec_agg_train)$sp[recalc_intact_valid],
+                            Run=meta(intact_spec_agg_train)$FiberRun[recalc_intact_valid],
+                            Measured=meta(intact_spec_agg_train)$recalcitrant[recalc_intact_valid],
+                            val_pred=recalc_intact$validation$pred[,,ncomp_recalc_intact])
+ggplot(recalc_intact_pred,aes(x=Measured,y=val_pred,color=Species))+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   theme_bw()+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
@@ -63,7 +82,7 @@ ggplot(ADF_intact_pred,aes(x=Measured,y=val_pred,color=Species))+
   theme(text = element_text(size=20),
         legend.position = c(0.8, 0.2))+
   labs(x="Measured",y="Predicted")+
-  ggtitle("Predicting %ADF from intact-leaf spectra")+guides(color=F)
+  ggtitle("Predicting % recalcitrants from intact-leaf spectra")+guides(color=F)
 
 ncomp_perC_intact <- selectNcomp(perC_intact, method = "onesigma", plot = FALSE)
 perC_intact_valid <- which(!is.na(meta(intact_spec_agg_train)$perC))
@@ -154,8 +173,9 @@ ggplot(perN_area_intact_pred,aes(x=Measured,y=val_pred,color=Species))+
 ## VIP plots
 
 source("VIP.R")
-VIP_intact<-data.frame(NDF=VIP(NDF_intact)[ncomp_NDF_intact,],
-                       ADF=VIP(ADF_intact)[ncomp_ADF_intact,],
+VIP_intact<-data.frame(sol=VIP(sol_intact)[ncomp_sol_intact,],
+                       hemi=VIP(hemi_intact)[ncomp_hemi_intact,],
+                       recalc=VIP(recalc_intact)[ncomp_recalc_intact,],
                        perC=VIP(perC_intact)[ncomp_perC_intact,],
                        perN=VIP(perN_intact)[ncomp_perN_intact,],
                        LMA=VIP(LMA_intact)[ncomp_LMA_intact,],
@@ -165,16 +185,18 @@ saveRDS(VIP_intact,"SavedResults/VIP_intact.rds")
 #######################################
 ## jackknife tests + prediction of validation data
 
-NDF_jack_coefs<-list()
-ADF_jack_coefs<-list()
+sol_jack_coefs<-list()
+hemi_jack_coefs<-list()
+recalc_jack_coefs<-list()
 perC_jack_coefs<-list()
 perN_jack_coefs<-list()
 perC_area_jack_coefs<-list()
 perN_area_jack_coefs<-list()
 LMA_jack_coefs<-list()
 
-NDF_jack_stats<-list()
-ADF_jack_stats<-list()
+sol_jack_stats<-list()
+hemi_jack_stats<-list()
+recalc_jack_stats<-list()
 perC_jack_stats<-list()
 perN_jack_stats<-list()
 perC_area_jack_stats<-list()
@@ -192,9 +214,11 @@ for(i in 1:nreps){
   calib_jack<-intact_spec_agg_train[train_jack]
   val_jack<-intact_spec_agg_train[test_jack]
   
-  NDF_intact_jack<-plsr(meta(calib_jack)$NDF~as.matrix(calib_jack),
+  sol_intact_jack<-plsr(meta(calib_jack)$solubles~as.matrix(calib_jack),
                         ncomp=30,method = "oscorespls",validation="none")
-  ADF_intact_jack<-plsr(meta(calib_jack)$ADF~as.matrix(calib_jack),
+  hemi_intact_jack<-plsr(meta(calib_jack)$hemicellulose~as.matrix(calib_jack),
+                        ncomp=30,method = "oscorespls",validation="none")
+  recalc_intact_jack<-plsr(meta(calib_jack)$recalcitrant~as.matrix(calib_jack),
                         ncomp=30,method = "oscorespls",validation="none")
   perC_intact_jack<-plsr(meta(calib_jack)$perC~as.matrix(calib_jack),
                          ncomp=30,method = "oscorespls",validation="none")
@@ -207,19 +231,26 @@ for(i in 1:nreps){
   LMA_intact_jack<-plsr(meta(calib_jack)$LMA~as.matrix(calib_jack),
                         ncomp=30,method = "oscorespls",validation="none")
   
-  NDF_jack_val_pred<-as.vector(predict(NDF_intact_jack,newdata=as.matrix(val_jack),ncomp=ncomp_NDF_intact)[,,1])
-  NDF_jack_val_fit<-lm(NDF_jack_val_pred~meta(val_jack)$NDF)
-  NDF_jack_stats[[i]]<-c(R2=summary(NDF_jack_val_fit)$r.squared,
-                         RMSE=RMSD(meta(val_jack)$NDF,NDF_jack_val_pred),
-                         perRMSE=percentRMSD(meta(val_jack)$NDF,NDF_jack_val_pred,0.025,0.975),
-                         bias=mean(NDF_jack_val_pred,na.rm=T)-mean(meta(val_jack)$NDF,na.rm=T))
+  sol_jack_val_pred<-as.vector(predict(sol_intact_jack,newdata=as.matrix(val_jack),ncomp=ncomp_sol_intact)[,,1])
+  sol_jack_val_fit<-lm(sol_jack_val_pred~meta(val_jack)$solubles)
+  sol_jack_stats[[i]]<-c(R2=summary(sol_jack_val_fit)$r.squared,
+                         RMSE=RMSD(meta(val_jack)$solubles,sol_jack_val_pred),
+                         perRMSE=percentRMSD(meta(val_jack)$solubles,sol_jack_val_pred,0.025,0.975),
+                         bias=mean(sol_jack_val_pred,na.rm=T)-mean(meta(val_jack)$solubles,na.rm=T))
+
+  hemi_jack_val_pred<-as.vector(predict(hemi_intact_jack,newdata=as.matrix(val_jack),ncomp=ncomp_hemi_intact)[,,1])
+  hemi_jack_val_fit<-lm(hemi_jack_val_pred~meta(val_jack)$hemicellulose)
+  hemi_jack_stats[[i]]<-c(R2=summary(hemi_jack_val_fit)$r.squared,
+                         RMSE=RMSD(meta(val_jack)$hemicellulose,hemi_jack_val_pred),
+                         perRMSE=percentRMSD(meta(val_jack)$hemicellulose,hemi_jack_val_pred,0.025,0.975),
+                         bias=mean(hemi_jack_val_pred,na.rm=T)-mean(meta(val_jack)$hemicellulose,na.rm=T))
   
-  ADF_jack_val_pred<-as.vector(predict(ADF_intact_jack,newdata=as.matrix(val_jack),ncomp=ncomp_ADF_intact)[,,1])
-  ADF_jack_val_fit<-lm(ADF_jack_val_pred~meta(val_jack)$ADF)
-  ADF_jack_stats[[i]]<-c(R2=summary(ADF_jack_val_fit)$r.squared,
-                         RMSE=RMSD(meta(val_jack)$ADF,ADF_jack_val_pred),
-                         perRMSE=percentRMSD(meta(val_jack)$ADF,ADF_jack_val_pred,0.025,0.975),
-                         bias=mean(ADF_jack_val_pred,na.rm=T)-mean(meta(val_jack)$ADF,na.rm=T))
+  recalc_jack_val_pred<-as.vector(predict(recalc_intact_jack,newdata=as.matrix(val_jack),ncomp=ncomp_recalc_intact)[,,1])
+  recalc_jack_val_fit<-lm(recalc_jack_val_pred~meta(val_jack)$recalcitrant)
+  recalc_jack_stats[[i]]<-c(R2=summary(recalc_jack_val_fit)$r.squared,
+                         RMSE=RMSD(meta(val_jack)$recalcitrant,recalc_jack_val_pred),
+                         perRMSE=percentRMSD(meta(val_jack)$recalcitrant,recalc_jack_val_pred,0.025,0.975),
+                         bias=mean(recalc_jack_val_pred,na.rm=T)-mean(meta(val_jack)$recalcitrant,na.rm=T))
   
   perC_jack_val_pred<-as.vector(predict(perC_intact_jack,newdata=as.matrix(val_jack),ncomp=ncomp_perC_intact)[,,1])
   perC_jack_val_fit<-lm(perC_jack_val_pred~meta(val_jack)$perC)
@@ -256,8 +287,9 @@ for(i in 1:nreps){
                          perRMSE=percentRMSD(meta(val_jack)$LMA,LMA_jack_val_pred,0.025,0.975),
                          bias=mean(LMA_jack_val_pred,na.rm=T)-mean(meta(val_jack)$LMA,na.rm=T))
   
-  NDF_jack_coefs[[i]]<-as.vector(coef(NDF_intact_jack,ncomp=ncomp_NDF_intact,intercept=TRUE))
-  ADF_jack_coefs[[i]]<-as.vector(coef(ADF_intact_jack,ncomp=ncomp_ADF_intact,intercept=TRUE))
+  sol_jack_coefs[[i]]<-as.vector(coef(sol_intact_jack,ncomp=ncomp_sol_intact,intercept=TRUE))
+  hemi_jack_coefs[[i]]<-as.vector(coef(hemi_intact_jack,ncomp=ncomp_hemi_intact,intercept=TRUE))
+  recalc_jack_coefs[[i]]<-as.vector(coef(recalc_intact_jack,ncomp=ncomp_recalc_intact,intercept=TRUE))
   perC_jack_coefs[[i]]<-as.vector(coef(perC_intact_jack,ncomp=ncomp_perC_intact,intercept=TRUE))
   perN_jack_coefs[[i]]<-as.vector(coef(perN_intact_jack,ncomp=ncomp_perN_intact,intercept=TRUE))
   perC_area_jack_coefs[[i]]<-as.vector(coef(perC_area_intact_jack,ncomp=ncomp_perC_area_intact,intercept=TRUE))
@@ -266,23 +298,33 @@ for(i in 1:nreps){
   
 }
 
-NDF_jack_pred<-apply.coefs(NDF_jack_coefs,as.matrix(intact_spec_agg_test))
-NDF_jack_stat<-t(apply(NDF_jack_pred,1,function(obs) c(mean(obs),quantile(obs,probs=c(0.025,0.975)))))
-NDF_jack_df<-data.frame(pred_mean=NDF_jack_stat[,1],
-                        pred_low=NDF_jack_stat[,2],
-                        pred_high=NDF_jack_stat[,3],
-                        Measured=meta(intact_spec_agg_test)$NDF,
-                        ncomp=ncomp_NDF_intact,
+sol_jack_pred<-apply.coefs(sol_jack_coefs,as.matrix(intact_spec_agg_test))
+sol_jack_stat<-t(apply(sol_jack_pred,1,function(obs) c(mean(obs),quantile(obs,probs=c(0.025,0.975)))))
+sol_jack_df<-data.frame(pred_mean=sol_jack_stat[,1],
+                        pred_low=sol_jack_stat[,2],
+                        pred_high=sol_jack_stat[,3],
+                        Measured=meta(intact_spec_agg_test)$solubles,
+                        ncomp=ncomp_sol_intact,
                         Species=meta(intact_spec_agg_test)$sp,
                         ID=meta(intact_spec_agg_test)$ID)
 
-ADF_jack_pred<-apply.coefs(ADF_jack_coefs,as.matrix(intact_spec_agg_test))
-ADF_jack_stat<-t(apply(ADF_jack_pred,1,function(obs) c(mean(obs),quantile(obs,probs=c(0.025,0.975)))))
-ADF_jack_df<-data.frame(pred_mean=ADF_jack_stat[,1],
-                        pred_low=ADF_jack_stat[,2],
-                        pred_high=ADF_jack_stat[,3],
-                        Measured=meta(intact_spec_agg_test)$ADF,
-                        ncomp=ncomp_ADF_intact,
+hemi_jack_pred<-apply.coefs(hemi_jack_coefs,as.matrix(intact_spec_agg_test))
+hemi_jack_stat<-t(apply(hemi_jack_pred,1,function(obs) c(mean(obs),quantile(obs,probs=c(0.025,0.975)))))
+hemi_jack_df<-data.frame(pred_mean=hemi_jack_stat[,1],
+                        pred_low=hemi_jack_stat[,2],
+                        pred_high=hemi_jack_stat[,3],
+                        Measured=meta(intact_spec_agg_test)$hemicellulose,
+                        ncomp=ncomp_hemi_intact,
+                        Species=meta(intact_spec_agg_test)$sp,
+                        ID=meta(intact_spec_agg_test)$ID)
+
+recalc_jack_pred<-apply.coefs(recalc_jack_coefs,as.matrix(intact_spec_agg_test))
+recalc_jack_stat<-t(apply(recalc_jack_pred,1,function(obs) c(mean(obs),quantile(obs,probs=c(0.025,0.975)))))
+recalc_jack_df<-data.frame(pred_mean=recalc_jack_stat[,1],
+                        pred_low=recalc_jack_stat[,2],
+                        pred_high=recalc_jack_stat[,3],
+                        Measured=meta(intact_spec_agg_test)$recalcitrant,
+                        ncomp=ncomp_recalc_intact,
                         Species=meta(intact_spec_agg_test)$sp,
                         ID=meta(intact_spec_agg_test)$ID)
 
@@ -342,8 +384,9 @@ LMA_jack_df<-data.frame(pred_mean=LMA_jack_stat[,1],
 intact_jack_coef_list<-list(LMA=LMA_jack_coefs,
                             perC=perC_jack_coefs,
                             perN=perN_jack_coefs,
-                            NDF=NDF_jack_coefs,
-                            ADF=ADF_jack_coefs,
+                            sol=sol_jack_coefs,
+                            hemi=hemi_jack_coefs,
+                            recalc=recalc_jack_coefs,
                             perC_area=perC_area_jack_coefs,
                             perN_area=perN_area_jack_coefs)
 saveRDS(intact_jack_coef_list,"SavedResults/intact_jack_coefs_list.rds")
@@ -351,8 +394,9 @@ saveRDS(intact_jack_coef_list,"SavedResults/intact_jack_coefs_list.rds")
 intact_jack_df_list<-list(LMA=LMA_jack_df,
                           perC=perC_jack_df,
                           perN=perN_jack_df,
-                          NDF=NDF_jack_df,
-                          ADF=ADF_jack_df,
+                          sol=sol_jack_df,
+                          hemi=hemi_jack_df,
+                          recalc=recalc_jack_df,
                           perC_area=perC_area_jack_df,
                           perN_area=perN_area_jack_df)
 saveRDS(intact_jack_df_list,"SavedResults/intact_jack_df_list.rds")
@@ -360,8 +404,9 @@ saveRDS(intact_jack_df_list,"SavedResults/intact_jack_df_list.rds")
 ############################################
 ## violin plots
 
-R2.df<-data.frame(NDF=unlist(lapply(NDF_jack_stats,function(x) x[["R2"]])),
-                  ADF=unlist(lapply(ADF_jack_stats,function(x) x[["R2"]])),
+R2.df<-data.frame(sol=unlist(lapply(sol_jack_stats,function(x) x[["R2"]])),
+                  hemi=unlist(lapply(hemi_jack_stats,function(x) x[["R2"]])),
+                  recalc=unlist(lapply(recalc_jack_stats,function(x) x[["R2"]])),
                   perC=unlist(lapply(perC_jack_stats,function(x) x[["R2"]])),
                   perN=unlist(lapply(perN_jack_stats,function(x) x[["R2"]])),
                   perC_area=unlist(lapply(perC_area_jack_stats,function(x) x[["R2"]])),
@@ -379,8 +424,9 @@ intact_val_R2<-ggplot(R2.long,aes(y=value,x=variable))+
   ggtitle("Intact-leaf spectra")+
   scale_y_continuous(expand = c(0, 0),limits=c(0,1))
 
-perRMSE.df<-data.frame(NDF=unlist(lapply(NDF_jack_stats,function(x) 100*x[["perRMSE"]])),
-                       ADF=unlist(lapply(ADF_jack_stats,function(x) 100*x[["perRMSE"]])),
+perRMSE.df<-data.frame(sol=unlist(lapply(sol_jack_stats,function(x) 100*x[["perRMSE"]])),
+                       hemi=unlist(lapply(hemi_jack_stats,function(x) 100*x[["perRMSE"]])),
+                       recalc=unlist(lapply(recalc_jack_stats,function(x) 100*x[["perRMSE"]])),
                        perC=unlist(lapply(perC_jack_stats,function(x) 100*x[["perRMSE"]])),
                        perN=unlist(lapply(perN_jack_stats,function(x) 100*x[["perRMSE"]])),
                        perC_area=unlist(lapply(perC_area_jack_stats,function(x) 100*x[["perRMSE"]])),
