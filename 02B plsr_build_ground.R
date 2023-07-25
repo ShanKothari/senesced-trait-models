9da4a9d57354140f9021ac53403f3d312366ed2d
Shan Kothari <quercusacerifolia@gmail.com>
2023-07-25 15:49:57 -0400

    change figure numbering, fix var names

--- 02B plsr_build_ground.R
+++ 02B plsr_build_ground.R
@@ -364,7 +364,7 @@ ground_val_perRMSE<-ggplot(perRMSE.long,aes(y=value,x=variable))+
   scale_y_continuous(expand = c(0, 0),
                      limits = c(0,max(perRMSE.long$value)*1.1))
 
-pdf("Manuscript/FigS2.pdf",height=8,width=4.5)
+pdf("Manuscript/FigS3.pdf",height=8,width=4.5)
 (ground_val_R2/ground_val_perRMSE)
 dev.off()
 
--- 03 plotting_model_output.R
+++ 03 plotting_model_output.R
@@ -41,7 +41,7 @@ VIP_ground_plot<-ggplot(VIP_ground_long,
   scale_x_continuous(expand = c(0, 0),limits=c(390,2410))+
   ylim(c(0,3))
 
-pdf("Manuscript/Fig5.pdf",height=8,width=7)
+pdf("Manuscript/Fig2.pdf",height=8,width=7)
 VIP_intact_plot/VIP_ground_plot+
   plot_layout(guides="collect") & theme(legend.position = "right")
 dev.off()
@@ -59,46 +59,46 @@ ground_jack_df_list<-readRDS("SavedResults/ground_jack_df_list.rds")
 ## define plot boundaries
 
 all.sol<-c(intact_jack_df_list$sol$Measured,
-            intact_jack_df_list$sol$pred_mean,
-            ground_jack_df_list$sol$pred_mean)
+           intact_jack_df_list$sol$pred_mean,
+           ground_jack_df_list$sol$pred_mean)
 sol_upper<-max(all.sol,na.rm=T)+3
 sol_lower<-min(all.sol,na.rm=T)-3
 
 all.hemi<-c(intact_jack_df_list$hemi$Measured,
-           intact_jack_df_list$hemi$pred_mean,
-           ground_jack_df_list$hemi$pred_mean)
+            intact_jack_df_list$hemi$pred_mean,
+            ground_jack_df_list$hemi$pred_mean)
 hemi_upper<-max(all.hemi,na.rm=T)+1
 hemi_lower<-min(all.hemi,na.rm=T)-1
 
 all.recalc<-c(intact_jack_df_list$recalc$Measured,
-           intact_jack_df_list$recalc$pred_mean,
-           ground_jack_df_list$recalc$pred_mean)
+              intact_jack_df_list$recalc$pred_mean,
+              ground_jack_df_list$recalc$pred_mean)
 recalc_upper<-max(all.recalc,na.rm=T)+2
 recalc_lower<-min(all.recalc,na.rm=T)-2
 
-all.perN<-c(intact_jack_df_list$perN$Measured,
-            intact_jack_df_list$perN$pred_mean,
-            ground_jack_df_list$perN$pred_mean)
-perN_upper<-max(all.perN,na.rm=T)+0.2
-perN_lower<-min(all.perN,na.rm=T)-0.2
+all.Nmass<-c(intact_jack_df_list$Nmass$Measured,
+             intact_jack_df_list$Nmass$pred_mean,
+             ground_jack_df_list$Nmass$pred_mean)
+Nmass_upper<-max(all.Nmass,na.rm=T)+0.2
+Nmass_lower<-min(all.Nmass,na.rm=T)-0.2
 
-all.perC<-c(intact_jack_df_list$perC$Measured,
-            intact_jack_df_list$perC$pred_mean,
-            ground_jack_df_list$perC$pred_mean)
-perC_upper<-max(all.perC,na.rm=T)+2
-perC_lower<-min(all.perC,na.rm=T)-2
+all.Cmass<-c(intact_jack_df_list$Cmass$Measured,
+             intact_jack_df_list$Cmass$pred_mean,
+             ground_jack_df_list$Cmass$pred_mean)
+Cmass_upper<-max(all.Cmass,na.rm=T)+2
+Cmass_lower<-min(all.Cmass,na.rm=T)-2
 
-all.perC_area<-c(intact_jack_df_list$perC_area$Measured,
-                 intact_jack_df_list$perC_area$pred_mean,
-                 ground_jack_df_list$perC_area$pred_mean)
-perC_area_upper<-max(all.perC_area,na.rm=T)+10
-perC_area_lower<-min(all.perC_area,na.rm=T)-10
+all.Carea<-c(intact_jack_df_list$Carea$Measured,
+             intact_jack_df_list$Carea$pred_mean,
+             ground_jack_df_list$Carea$pred_mean)
+Carea_upper<-max(all.Carea,na.rm=T)+10
+Carea_lower<-min(all.Carea,na.rm=T)-10
 
-all.perN_area<-c(intact_jack_df_list$perN_area$Measured,
-                 intact_jack_df_list$perN_area$pred_mean,
-                 ground_jack_df_list$perN_area$pred_mean)
-perN_area_upper<-max(all.perN_area,na.rm=T)+0.2
-perN_area_lower<-min(all.perN_area,na.rm=T)-0.2
+all.Narea<-c(intact_jack_df_list$Narea$Measured,
+             intact_jack_df_list$Narea$pred_mean,
+             ground_jack_df_list$Narea$pred_mean)
+Narea_upper<-max(all.Narea,na.rm=T)+0.2
+Narea_lower<-min(all.Narea,na.rm=T)-0.2
 
 all.LMA<-c(intact_jack_df_list$LMA$Measured,
            intact_jack_df_list$LMA$pred_mean,
@@ -110,7 +110,7 @@ LMA_lower<-min(all.LMA,na.rm=T)-25
 ## create plots
 
 sol_intact_val_plot<-ggplot(intact_jack_df_list$sol,
-                             aes(y=Measured,x=pred_mean,color=Species))+
+                            aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
@@ -124,7 +124,7 @@ sol_intact_val_plot<-ggplot(intact_jack_df_list$sol,
   ggtitle("Intact")
 
 hemi_intact_val_plot<-ggplot(intact_jack_df_list$hemi,
-                            aes(y=Measured,x=pred_mean,color=Species))+
+                             aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
@@ -137,7 +137,7 @@ hemi_intact_val_plot<-ggplot(intact_jack_df_list$hemi,
   guides(color=F)
 
 recalc_intact_val_plot<-ggplot(intact_jack_df_list$recalc,
-                            aes(y=Measured,x=pred_mean,color=Species))+
+                               aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
@@ -149,14 +149,14 @@ recalc_intact_val_plot<-ggplot(intact_jack_df_list$recalc,
   labs(y="Measured recalcalcitrants (%)",x="Predicted recalcitrants (%)")+
   guides(color=F)
 
-perC_intact_val_plot<-ggplot(intact_jack_df_list$perC,
-                             aes(y=Measured,x=pred_mean,color=Species))+
+Cmass_intact_val_plot<-ggplot(intact_jack_df_list$Cmass,
+                              aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
-  coord_cartesian(xlim=c(perC_lower,perC_upper),ylim=c(perC_lower,perC_upper))+
+  coord_cartesian(xlim=c(Cmass_lower,Cmass_upper),ylim=c(Cmass_lower,Cmass_upper))+
   theme(text = element_text(size=20),
         legend.position = c(0.8, 0.25))+
   labs(y=expression("Measured C"[mass]*" (%)"),
@@ -164,44 +164,44 @@ perC_intact_val_plot<-ggplot(intact_jack_df_list$perC,
   guides(color=F)+
   ggtitle("Intact")
 
-perN_intact_val_plot<-ggplot(intact_jack_df_list$perN,
-                             aes(y=Measured,x=pred_mean,color=Species))+
+Nmass_intact_val_plot<-ggplot(intact_jack_df_list$Nmass,
+                              aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
-  coord_cartesian(xlim=c(perN_lower,perN_upper),ylim=c(perN_lower,perN_upper))+
+  coord_cartesian(xlim=c(Nmass_lower,Nmass_upper),ylim=c(Nmass_lower,Nmass_upper))+
   theme(text = element_text(size=20),
         legend.position = c(0.8, 0.25))+
   labs(y=expression("Measured N"[mass]*" (%)"),
        x=expression("Predicted N"[mass]*" (%)"))+
   guides(color=F)
 
-perC_area_intact_val_plot<-ggplot(intact_jack_df_list$perC_area,
-                                  aes(y=Measured,x=pred_mean,color=Species))+
+Carea_intact_val_plot<-ggplot(intact_jack_df_list$Carea,
+                              aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
-  coord_cartesian(c(perC_area_lower,perC_area_upper),
-                  c(perC_area_lower,perC_area_upper))+
+  coord_cartesian(c(Carea_lower,Carea_upper),
+                  c(Carea_lower,Carea_upper))+
   theme(text = element_text(size=20),
         legend.position = c(0.8, 0.25))+
   labs(y=expression("Measured C"[area]*" (g/m"^2*")"),
        x=expression("Predicted C"[area]*" (g/m"^2*")"))+
   ggtitle("Intact")
 
-perN_area_intact_val_plot<-ggplot(intact_jack_df_list$perN_area,
-                                  aes(y=Measured,x=pred_mean,color=Species))+
+Narea_intact_val_plot<-ggplot(intact_jack_df_list$Narea,
+                              aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
-  coord_cartesian(c(perN_area_lower,perN_area_upper),
-                  c(perN_area_lower,perN_area_upper))+
+  coord_cartesian(c(Narea_lower,Narea_upper),
+                  c(Narea_lower,Narea_upper))+
   theme(text = element_text(size=20),
         legend.position = c(0.8, 0.25))+
   labs(y=expression("Measured N"[area]*" (g/m"^2*")"),
@@ -223,7 +223,7 @@ LMA_intact_val_plot<-ggplot(intact_jack_df_list$LMA,
   guides(color=F)
 
 sol_ground_val_plot<-ggplot(ground_jack_df_list$sol,
-                             aes(y=Measured,x=pred_mean,color=Species))+
+                            aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
@@ -239,7 +239,7 @@ sol_ground_val_plot<-ggplot(ground_jack_df_list$sol,
   ggtitle("Ground")
 
 hemi_ground_val_plot<-ggplot(ground_jack_df_list$hemi,
-                            aes(y=Measured,x=pred_mean,color=Species))+
+                             aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
@@ -267,14 +267,14 @@ recalc_ground_val_plot<-ggplot(ground_jack_df_list$recalc,
         axis.text.y = element_blank())+
   labs(y="Measured recalcitrants (%)",x="Predicted recalcitrants (%)")
 
-perC_ground_val_plot<-ggplot(ground_jack_df_list$perC,
-                             aes(y=Measured,x=pred_mean,color=Species))+
+Cmass_ground_val_plot<-ggplot(ground_jack_df_list$Cmass,
+                              aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
-  coord_cartesian(xlim=c(perC_lower,perC_upper),ylim=c(perC_lower,perC_upper))+
+  coord_cartesian(xlim=c(Cmass_lower,Cmass_upper),ylim=c(Cmass_lower,Cmass_upper))+
   theme(text = element_text(size=20),
         legend.position = c(0.8, 0.25),
         axis.title.y = element_blank(),
@@ -284,14 +284,14 @@ perC_ground_val_plot<-ggplot(ground_jack_df_list$perC,
   guides(color=F)+
   ggtitle("Ground")
 
-perN_ground_val_plot<-ggplot(ground_jack_df_list$perN,
-                             aes(y=Measured,x=pred_mean,color=Species))+
+Nmass_ground_val_plot<-ggplot(ground_jack_df_list$Nmass,
+                              aes(y=Measured,x=pred_mean,color=Species))+
   theme_bw()+
   geom_errorbarh(aes(y=Measured,xmin=pred_low,xmax=pred_high),
                  color="darkslategrey",alpha=0.7)+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
-  coord_cartesian(xlim=c(perN_lower,perN_upper),ylim=c(perN_lower,perN_upper))+
+  coord_cartesian(xlim=c(Nmass_lower,Nmass_upper),ylim=c(Nmass_lower,Nmass_upper))+
   theme(text = element_text(size=20),
         legend.position = c(0.8, 0.25),
         axis.title.y = element_blank(),
@@ -318,19 +318,19 @@ LMA_ground_val_plot<-ggplot(ground_jack_df_list$LMA,
 ################################
 ## arrange plots
 
-pdf("Manuscript/Fig2.pdf",height=13,width=10)
+pdf("Manuscript/Fig3.pdf",height=13,width=10)
 (sol_intact_val_plot/hemi_intact_val_plot/recalc_intact_val_plot)|
   (sol_ground_val_plot/hemi_ground_val_plot/recalc_ground_val_plot) +
   plot_layout(guides="collect") & theme(legend.position = "right")
 dev.off()
 
-pdf("Manuscript/Fig3.pdf",height=13,width=10)
-(perC_intact_val_plot/perN_intact_val_plot/LMA_intact_val_plot)|
-  (perC_ground_val_plot/perN_ground_val_plot/LMA_ground_val_plot) +
+pdf("Manuscript/Fig4.pdf",height=13,width=10)
+(Cmass_intact_val_plot/Nmass_intact_val_plot/LMA_intact_val_plot)|
+  (Cmass_ground_val_plot/Nmass_ground_val_plot/LMA_ground_val_plot) +
   plot_layout(guides="collect") & theme(legend.position = "right")
 dev.off()
 
-pdf("Manuscript/Fig4.pdf",height=8,width=5.5)
-(perC_area_intact_val_plot/perN_area_intact_val_plot) +
+pdf("Manuscript/Fig5.pdf",height=8,width=5.5)
+(Carea_intact_val_plot/Narea_intact_val_plot) +
   plot_layout(guides="collect") & theme(legend.position = "right")
 dev.off()
